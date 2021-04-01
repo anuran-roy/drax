1330ce6df24529c77b6e0e6f33e8f00a17cbef49
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2021-04-01 21:01:25 +0530

    Added few files
    Added the proxy fetcher modules, and the main script.

---
+++ drax.py
@@ -0,0 +1,44 @@
+#import mechanize as mz
+#import time as t
+from pyfiglet import Figlet
+import sys
+import os
+from importlib import import_module as im
+
+try:
+    custom_fig = Figlet(font='graffiti')
+    print(custom_fig.renderText('Drax'))
+except:
+    print("Drax")   
+print("\nBrute force with a mind")     
+print("\nAuthor: radioactvt\n")
+cmd_disp = {
+    'about':'./about.txt',
+    'help':'./help.txt',
+}
+cmd_disp_set=cmd_disp.keys()
+cmd_exec = {
+    'fetch proxy':'modules.proxy_fetch',
+    'start bruteforcing':'modules.bruteforce'
+}
+cmd_exec_set=cmd_exec.keys()
+while(True):
+    cmd=input('drax> ')
+    if cmd=='exit' or cmd=='quit':
+        print("Exiting Drax...")
+        sys.exit()
+
+    if cmd in cmd_disp_set:
+        try:    
+            print(open(cmd_disp[cmd],'r').read())
+        except Exception as e:
+            print(f"Error occured while trying to execute command {cmd}. Error message: {e}")    
+        continue
+
+    if cmd in cmd_exec_set:
+        try:    
+            #exec(open(cmd_exec[cmd],'r').read())
+            im(cmd_exec[cmd]).run()
+        except Exception as e:
+            print(f"Error occured while trying to execute command {cmd}. Error message: {e}")
+        continue
---
+++ modules/proxy_fetch.py
@@ -0,0 +1,77 @@
+import requests
+from lxml.html import fromstring
+import sys
+from datetime import datetime
+import os
+class ProxyError(Exception):
+    '''Proxy source(s) not found.'''
+    pass
+
+def get_proxies():
+    try:
+        urls = [
+            'https://free-proxy-list.net/'
+        ]
+
+        proxies = set()
+        for url in urls:
+            response = requests.get(url)
+            parser = fromstring(response.text)
+            
+            for i in parser.xpath('//tbody/tr')[:10]:
+                if i.xpath('.//td[7][contains(text(),"yes")]'):
+                    #Grabbing IP and corresponding PORT
+                    proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+                    proxies.add(proxy)
+        return proxies
+    except:
+        raise ProxyError
+        return None        
+
+def read_from_file(path):
+    try:
+        fopen=open(path,'r');
+        proxies=fopen.readlines()
+        return proxies
+    except:
+        raise ProxyError
+        return None    
+
+def write_to_file(addr):
+    try:
+        nn="./modules/proxies"+str(datetime.now().strftime("%d-%m-%Y-%H-%M-%S"))+".txt"
+        os.rename("./modules/proxies.txt",nn)
+        fw=open('./modules/proxies.txt','a')
+        addr=list(addr)
+        fw.writelines(addr)
+        fw.close()
+    except Exception as ex:
+        print(f"Error writing to buffer file. Error details: {ex}")    
+
+def run():
+    while True:
+        prx=input("drax>proxy: ")
+        if prx=="exit":
+            print("Exiting Drax...")
+            sys.exit()
+        if prx=="back":
+            break
+        try:
+            if prx=="help":
+                print(open("./modules/proxy_help.txt","r").read())
+            else:
+                if prx=="get online":
+                    print("Getting proxies from online")
+                    resp=get_proxies()
+                    if resp is not None:
+                        write_to_file(resp)
+                    else:
+                        print("There's some problem with scraping the IP addresses.")
+                elif prx=="read file":
+                    resp=read_from_file(input("Enter absolute path of IP addresses."))
+                    if resp is not None:
+                        write_to_file(resp)
+                    else:
+                        print("There's some problem with reading the file.")    
+        except Exception as e:
+            print(f"Some error occured! Error details: {e}")
